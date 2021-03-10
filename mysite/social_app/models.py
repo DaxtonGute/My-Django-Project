42f4868682e918b3ccdea73cc3c3d909d8ccd96c
DaxGute <daxton@gutekunst.com>
2021-03-09 23:13:16 -0800

    I was j unable to make the admin behave, it was too cloudy for me

--- mysite/social_app/models.py
+++ mysite/social_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
-# Create your models here.
+class YourMessage(models.Model):
+    Message_Text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.Message_Text
+
+
+class MyMessage(models.Model):
+    Message_Text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.Message_Text
+
+class ConvoPreview(models.Model):
+    Message_Text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.Message_Text

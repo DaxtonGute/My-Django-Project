85f2ba185fd5409715f5c24527dcedcbbb66ac4b
DaxGute <daxton@gutekunst.com>
2021-03-22 11:41:04 -0700

    I did some stuff

--- mysite/social_app/views.py
+++ mysite/social_app/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+isGuest = False
 
 def HomePage(request):
-    return render(request,'social_app/HomePage.html')
+    logIn = request.POST.get('logIn')
+    logOut = request.POST.get('logOut')
+    switch = request.POST.get('switchViews')
+    isGuest = False #how to access the global variable isGuest
+    if logIn:
+        isGuest = False
+    if logOut:
+        isGuest = True
+    if switch:
+        isGuest = not isGuest
+
+    print("hi")
+
+    return render(request,'social_app/HomePage.html',{'isGuest': isGuest})
 
 def Messages(request):
     return render(request,'social_app/Messages.html')

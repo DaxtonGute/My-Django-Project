6ead7cd73d68c778ec38ebedd426c7fe465230d5
DaxGute <daxton@gutekunst.com>
2021-03-24 09:23:22 -0700

    I did some stuff
     I don't quite understand models but whatever

--- mysite/social_app/models.py
+++ mysite/social_app/models.py
@@ -4,19 +4,25 @@ from django.utils import timezone
 
 class YourMessage(models.Model):
     Message_Text = models.CharField(max_length=200)
+    Time_Stamp = models.DateField()
 
     def __str__(self):
         return self.Message_Text
+        return self.Time_Stamp
 
 
 class MyMessage(models.Model):
     Message_Text = models.CharField(max_length=200)
+    Time_Stamp = models.DateField()
 
     def __str__(self):
         return self.Message_Text
+        return self.Time_Stamp
 
 class ConvoPreview(models.Model):
     Message_Text = models.CharField(max_length=200)
+    Group_Name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.Message_Text
+        return self.Group_Name
